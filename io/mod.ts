2768de2f38e1a95c579d6933ecf3a4615737d0b5
Janis Pritzkau <janispritzkau@gmail.com>
2022-11-02 20:33:41 +0100

    Add compression support

--- io/mod.ts
+++ io/mod.ts
@@ -1,2 +1,11 @@
-export { Reader } from "./reader.ts"
-export { Writer } from "./writer.ts"
+export { Reader } from "./reader.ts";
+export { Writer } from "./writer.ts";
+
+export function getVarIntSize(x: number) {
+  let n = 0;
+  do {
+    x >>>= 7;
+    n += 1;
+  } while (x != 0);
+  return n;
+}
