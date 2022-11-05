7b9950479e4c49a5b31f3d2d650e7df96b162bc9
Janis Pritzkau <janispritzkau@gmail.com>
2022-11-05 21:20:23 +0100

    Add module documentation

--- README.md
+++ README.md
@@ -8,15 +8,15 @@ Emphasis is on usability, ease of use, and having well-written tests to improve 
 
 ## Modules
 
-### [`chat/`](chat/)
+### [`chat/`](https://deno.land/x/minecraft_lib/chat/)
 
 Functions and types for working with chat components. **Incomplete, types only**.
 
-### [`io/`](io/)
+### [`io/`](https://deno.land/x/minecraft_lib/io/)
 
 Helper classes and functions for writing and reading binary data. Includes a `Writer` and `Reader` class.
 
-### [`nbt/`](nbt/)
+### [`nbt/`](https://deno.land/x/minecraft_lib/nbt/)
 
 An implementation of Minecraft's named binary tag (NBT) format. Including:
 
@@ -26,7 +26,7 @@ An implementation of Minecraft's named binary tag (NBT) format. Including:
 
 Implementation of NBT-Path is also planned.
 
-### [`network/`](network/)
+### [`network/`](https://deno.land/x/minecraft_lib/network/)
 
 Primitives for building clients and servers. This includes a `Connection` class which handles the framing of packets, and can be used in combination with a `Protocol` definition for the automatic encoding/decoding of packets.
 
--- network/packet.ts
+++ network/packet.ts
@@ -1,7 +1,40 @@
 import { Reader, Writer } from "../io/mod.ts";
 
+/**
+ * The base interface packets need to implement.
+ *
+ * In order for a packet to be used in a {@linkcode Protocol}, it must also
+ * implement a static read method as defined in {@linkcode PacketConstructor}.
+ *
+ * ### Example packet definition
+ *
+ * ```ts
+ * class ClientStatusResponsePacket implements Packet<ClientStatusHandler> {
+ *   constructor(public status: unknown) {}
+ *
+ *   static read(reader: Reader) {
+ *     return new this(reader.readJSON());
+ *   }
+ *
+ *   write(writer: Writer) {
+ *     writer.writeJSON(this.status);
+ *   }
+ *
+ *   handle(handler: ClientStatusHandler) {
+ *     return handler.handleResponse(this);
+ *   }
+ * }
+ * ```
+ */
 export interface Packet<Handler = void> {
   write(writer: Writer): void;
+  /**
+   * Gets called on {@linkcode Connection.receive} when a protocol and
+   * packet handler is set on the {@linkcode Connection} instance.
+   *
+   * The `handler` parameter is the packet handler specified via
+   * {@linkcode Connection.setServerProtocol} or {@linkcode Connection.setClientProtocol}.
+   */
   handle(handler: Handler): Promise<void> | void;
 }
 
@@ -12,6 +45,20 @@ export interface PacketConstructor<P extends Packet<unknown>> {
   read(reader: Reader): P;
 }
 
+/**
+ * The base interface for a connection packet handler.
+ *
+ * A packet handler may look something like this:
+ *
+ * ```ts
+ * interface ClientGameHandler extends PacketHandler {
+ *   handleDisconnect?(packet: ClientDisconnectPacket): Promise<void>;
+ *   handleKeepAlive?(packet: ClientKeepAlivePacket): Promise<void>;
+ *   // ... methods for all the other packets
+ * }
+ * ```
+ */
 export interface PacketHandler {
+  /** Gets called when the connection is closed. */
   onDisconnect?(): void;
 }
